e0524ea50d17cc7559390209523421de585a0fd6
Mateusz <wojtowiczm1014@gmail.com>
2023-06-14 00:49:32 +0200

    fix: offline users script

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py shell < ./shell/set_users_offline.py
 
 daphne -b 0.0.0.0 -p 8000 django_chess.asgi:application
